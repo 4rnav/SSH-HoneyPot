f761cb6950cbb00008841a1fdc4fcdb4d09b8b74
Unshadowed <89694754+Atul-Nair@users.noreply.github.com>
2023-04-05 21:46:05 +0530

    start_server function added with main function

--- ssh_honeypot.py
+++ ssh_honeypot.py
@@ -213,92 +213,39 @@ def handle_connection(client, addr):
             pass
 
 
-def handle_connection(client, addr):
-    client_ip = addr[0]
-    logging.info('New connection from: {}'.format(client_ip))
-    print('New connection from: {}'.format(client_ip))
-
+def start_server(port, bind):
+    """Init and run the ssh server"""
     try:
-        transport = paramiko.Transport(client)
-        transport.add_server_key(HOST_KEY)
-        transport.local_version = SSH_BANNER # Change banner to appear more convincing
-        server = BasicSshHoneypot(client_ip)
-        try:
-            transport.start_server(server=server)
-
-        except paramiko.SSHException:
-            print('*** SSH negotiation failed.')
-            raise Exception("SSH negotiation failed")
-
-        # wait for auth
-        chan = transport.accept(10)
-        if chan is None:
-            print('*** No channel (from '+client_ip+').')
-            raise Exception("No channel")
-        
-        chan.settimeout(10)
-
-        if transport.remote_mac != '':
-            logging.info('Client mac ({}): {}'.format(client_ip, transport.remote_mac))
-
-        if transport.remote_compression != '':
-            logging.info('Client compression ({}): {}'.format(client_ip, transport.remote_compression))
-
-        if transport.remote_version != '':
-            logging.info('Client SSH version ({}): {}'.format(client_ip, transport.remote_version))
-            
-        if transport.remote_cipher != '':
-            logging.info('Client SSH cipher ({}): {}'.format(client_ip, transport.remote_cipher))
-
-        server.event.wait(10)
-        if not server.event.is_set():
-            logging.info('** Client ({}): never asked for a shell'.format(client_ip))
-            raise Exception("No shell request")
-     
-        try:
-            chan.send("Welcome to Ubuntu 18.04.4 LTS (GNU/Linux 4.15.0-128-generic x86_64)\r\n\r\n")
-            run = True
-            while run:
-                chan.send("$ ")
-                command = ""
-                while not command.endswith("\r"):
-                    transport = chan.recv(1024)
-                    print(client_ip+"- received:",transport)
-                    # Echo input to psuedo-simulate a basic terminal
-                    if(
-                        transport != UP_KEY
-                        and transport != DOWN_KEY
-                        and transport != LEFT_KEY
-                        and transport != RIGHT_KEY
-                        and transport != BACK_KEY
-                    ):
-                        chan.send(transport)
-                        command += transport.decode("utf-8")
-                
-                chan.send("\r\n")
-                command = command.rstrip()
-                logging.info('Command received ({}): {}'.format(client_ip, command))
-
-                if command == "exit":
-                    logging.info('Connection closed (via exit command): {}'.format(client_ip))
-                    run = False
-
-                else:
-                    handle_cmd(command, chan, client_ip)
-
-        except Exception as err:
-            print('!!! Exception: {}: {}'.format(err.__class__, err))
-            try:
-                transport.close()
-            except Exception:
-                pass
-
-        chan.close()
-
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind((bind, port))
     except Exception as err:
-        print('!!! Exception: {}: {}'.format(err.__class__, err))
+        print('*** Bind failed: {}'.format(err))
+        traceback.print_exc()
+        sys.exit(1)
+
+    threads = []
+    while True:
         try:
-            transport.close()
-        except Exception:
-            pass
+            sock.listen(100)
+            print('Listening for connection on port {} ...'.format(port))
+            client, addr = sock.accept()
+        except Exception as err:
+            print('*** Listen/accept failed: {}'.format(err))
+            traceback.print_exc()
+        new_thread = threading.Thread(target=handle_connection, args=(client, addr))
+        new_thread.start()
+        threads.append(new_thread)
+
+    for thread in threads:
+        thread.join()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Run an SSH honeypot server')
+    parser.add_argument("--port", "-p", help="The port to bind the ssh server to (default 22)", default=2222, type=int, action="store")
+    parser.add_argument("--bind", "-b", help="The address to bind the ssh server to", default="", type=str, action="store")
+    args = parser.parse_args()
+    start_server(args.port, args.bind)
+
 

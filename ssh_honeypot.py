7a2d290b30571035a1e928000e5aaea7078c179b
Unshadowed <89694754+Atul-Nair@users.noreply.github.com>
2023-04-05 21:17:08 +0530

    Added BasicSshHoneypot class

--- ssh_honeypot.py
+++ ssh_honeypot.py
@@ -76,3 +76,49 @@ def handle_cmd(cmd, chan, ip):
         logging.info('Response from honeypot ({}): '.format(ip, response))
         response = response + "\r\n"
     chan.send(response)
+
+
+class BasicSshHoneypot(paramiko.ServerInterface):
+
+    client_ip = None
+
+    def __init__(self, client_ip):
+        self.client_ip = client_ip
+        self.event = threading.Event()
+
+    def check_channel_request(self, kind, chanid):
+        logging.info('client called check_channel_request ({}): {}'.format(
+                    self.client_ip, kind))
+        if kind == 'session':
+            return paramiko.OPEN_SUCCEEDED
+
+    def get_allowed_auths(self, username):
+        logging.info('client called get_allowed_auths ({}) with username {}'.format(
+                    self.client_ip, username))
+        return "publickey,password"
+
+    def check_auth_publickey(self, username, key):
+        fingerprint = u(hexlify(key.get_fingerprint()))
+        logging.info('client public key ({}): username: {}, key name: {}, md5 fingerprint: {}, base64: {}, bits: {}'.format(
+                    self.client_ip, username, key.get_name(), fingerprint, key.get_base64(), key.get_bits()))
+        return paramiko.AUTH_PARTIALLY_SUCCESSFUL        
+
+    def check_auth_password(self, username, password):
+        # Accept all passwords as valid by default
+        logging.info('new client credentials ({}): username: {}, password: {}'.format(
+                    self.client_ip, username, password))
+        return paramiko.AUTH_SUCCESSFUL
+
+    def check_channel_shell_request(self, channel):
+        self.event.set()
+        return True
+
+    def check_channel_pty_request(self, channel, term, width, height, pixelwidth, pixelheight, modes):
+        return True
+
+    def check_channel_exec_request(self, channel, command):
+        command_text = str(command.decode("utf-8"))
+        handle_cmd(command_text, channel, self.client_ip)
+        logging.info('client sent command via check_channel_exec_request ({}): {}'.format(
+                    self.client_ip, command))
+        return True
